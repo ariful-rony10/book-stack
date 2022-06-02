3ea685b8c27b86704230dc1d7e52984782ddec8e
Md. Ariful Islam <ariful.rony10@gmail.com>
2022-06-03 03:27:30 +0600

    author added

---
+++ routes/author.js
@@ -0,0 +1,32 @@
+const express = require('express');
+const router = express.Router();
+const Author = require('../models/author');
+
+// All Authors Route
+router.get('/', (req, res) => {
+  res.render('authors/index');
+});
+// New Author Route
+router.get('/new', (req, res) => {
+  res.render('authors/new', { author: new Author() });
+});
+// Creating New Author Route
+router.post('/', (req, res) => {
+  const author = new Author({
+    name: req.body.name
+  })
+  author.save((err, newAuthor) => {
+    if(err) {
+      res.render('authors/new', {
+        author: author,
+        errorMessage: 'Error Creating Author'
+      }) 
+    } else {
+      // res.redirect(`authors/${newAuthor.id}`)
+      res.send(`authors`)
+    }
+  });
+  res.send(req.body.name);
+});
+
+module.exports = router;
--- server.js
+++ server.js
@@ -1,31 +1,30 @@
 if (process.env.NODE_ENV !== 'production') {
-  require('dotenv').config()
+  require('dotenv').config();
 }
 // Dependencies
 const express = require('express');
 const app = express();
 const expressLayouts = require('express-ejs-layouts');
 const path = require('path');
-
+const bodyParser = require('body-parser');
 
 const indexRouter = require('./routes/index');
+const authorRouter = require('./routes/author');
 
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 app.set('layout', 'layouts/layout');
 app.use(expressLayouts);
 app.use(express.static(path.join(__dirname, 'public')));
-
+app.use(bodyParser.urlencoded({ limit: '10mb', extended: false }));
 
 const mongoose = require('mongoose');
 mongoose.connect(process.env.DATABASE_URL);
 const db = mongoose.connection;
-db.on('error', error => console.error(error))
-db.once('open', () => console.log('Connected to Mongoose'))
-
-
+db.on('error', (error) => console.error(error));
+db.once('open', () => console.log('Connected to Mongoose'));
 
 app.use('/', indexRouter);
+app.use('/authors', authorRouter);
 
-
-app.listen(process.env.PORT || 8080);
+app.listen(process.env.PORT || 5500);
