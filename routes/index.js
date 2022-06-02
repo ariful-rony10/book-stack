3881a05c0bd95bfbe04888aa13976e0668f55cdd
Md. Ariful Islam <ariful.rony10@gmail.com>
2022-06-03 01:50:20 +0600

    folder structure created

---
+++ routes/index.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const router = express.Router();
+
+router.get('/', (req, res) => {
+  res.send('Hello World! - Anything')
+});
+
+
+module.exports = router;
+
+
---
+++ server.js
@@ -0,0 +1,21 @@
+// Dependencies
+const express = require('express');
+const app = express();
+const expressLayouts = require('express-ejs-layouts');
+const path = require('path');
+
+
+const indexRouter = require('./routes/index');
+
+
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+app.set('layout', 'layouts/layout');
+app.use(expressLayouts);
+app.use(express.static(path.join(__dirname, 'public')));
+
+
+app.use('/', indexRouter);
+
+
+app.listen(process.env.PORT || 3000);
