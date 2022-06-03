c947cfa7d351dfe01cb13d1230e3edabb2d9a696
Md. Ariful Islam <ariful.rony10@gmail.com>
2022-06-03 14:26:03 +0600

    author search working

--- routes/author.js
+++ routes/author.js
@@ -3,29 +3,41 @@ const router = express.Router();
 const Author = require('../models/author');
 
 // All Authors Route
-router.get('/', (req, res) => {
-  res.render('authors/index');
+router.get('/', async (req, res) => {
+  let searchOptions = {};
+  if (req.query.name != null && req.query.name !== '') {
+    searchOptions.name = new RegExp(req.query.name, 'i');
+  }
+  try {
+    const authors = await Author.find(searchOptions);
+    res.render('authors/index', { 
+      authors: authors, 
+      searchOptions: req.query 
+    });
+  } catch {
+    res.redirect('/');
+  }
 });
+
 // New Author Route
 router.get('/new', (req, res) => {
   res.render('authors/new', { author: new Author() });
 });
 // Creating New Author Route
-router.post('/', (req, res) => {
+router.post('/', async (req, res) => {
   const author = new Author({
-    name: req.body.name
-  })
-  author.save((err, newAuthor) => {
-    if(err) {
-      res.render('authors/new', {
-        author: author,
-        errorMessage: 'Error Creating Author'
-      }) 
-    } else {
-      // res.redirect(`authors/${newAuthor.id}`)
-      res.send(`authors`)
-    }
+    name: req.body.name,
   });
+  try {
+    const newAuthor = await author.save();
+    // res.redirect(`authors/${newAuthor.id}`)
+    res.send(`authors`);
+  } catch (error) {
+    res.render('authors/new', {
+      author: author,
+      errorMessage: 'Error Creating Author',
+    });
+  }
   res.send(req.body.name);
 });
 
